3e44da21bdb4531ded6f8621568e1e7d29c7de15
Rosyidimalang <ahmadrosyidi432@gmail.com>
2023-01-04 15:10:54 +0700

    alhamduillah

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const btns = [
-  "1",
-  "2",
-  "3",
-  "+",
-  "4",
-  "5",
-  "6",
-  "-",
-  "7",
-  "8",
-  "9",
-  "*",
-  ".",
-  "0",
-  "=",
-  "/",
-];
+const btns1 = ["1", "2", "3", "+"];
+const btns2 = ["4", "5", "6", "-"];
+const btns3 = ["7", "8", "9", "*"];
+const btns4 = [".", "0", "=", ":"];
 
 function App() {
   const [result, setResult] = useState("");
@@ -41,85 +27,48 @@ function App() {
 
   return (
     <div className="">
-      <input className="text-9xl font-bold" type="text" value={result} />
-      <div className="grid grid-cols-3">
-        {btns.map((btn, idx) => (
+      <input className="text-9xl font-bold " type="text" value={result} />
+
+      <div className="flex justify-center">
+        {btns1.map((btn, idx) => (
           <button
-            className="border border-yellow-400 rounded h-20"
+            className="border border-slate-100 rounded w-48 h-20 bg-gray-900 text-white text-5xl"
             onClick={() => handleClick(btn)}
           >
-            {btn === "*" ? "x" : btn === "/" ? "÷" : btn}
+            {btn}
           </button>
         ))}
       </div>
-
-      <div className="font-bold text-7xl text-yellow-400">text</div>
-
-      <table>
-        <tbody>
-          <tr>
-            <td colSpan="4">
-              <input type="text" value={result} />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => handleClick("1")}>1</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("2")}>2</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("3")}>3</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("+")}>+</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => handleClick("4")}>4</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("5")}>5</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("6")}>6</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("-")}>-</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => handleClick("7")}>7</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("8")}>8</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("9")}>9</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("*")}>x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => handleClick(".")}>.</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("0")}>0</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("=")}>=</button>
-            </td>
-            <td>
-              <button onClick={() => handleClick("/")}>÷</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="flex justify-center">
+        {btns2.map((btn, idx) => (
+          <button
+            className="border border-slate-100 rounded w-48 h-20 bg-gray-900 text-white text-5xl"
+            onClick={() => handleClick(btn)}
+          >
+            {btn}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        {btns3.map((btn, idx) => (
+          <button
+            className="border border-slate-100 rounded w-48 h-20 bg-gray-900 text-white text-5xl"
+            onClick={() => handleClick(btn)}
+          >
+            {btn === "*" ? "x" : btn}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        {btns4.map((btn, idx) => (
+          <button
+            className="border border-slate-100 rounded w-48 h-20 bg-gray-900 text-white text-5xl"
+            onClick={() => handleClick(btn)}
+          >
+            {btn === ":" ? "÷" : btn}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
